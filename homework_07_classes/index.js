41d5f66d9de947262034ea8ccae8b0ec15ca7a35
PavelSproot <forregs@inbox.ru>
2021-10-20 00:26:44 +1000

    Сделана третья часть - наследование через классы.

--- homework_07_classes/index.js
+++ homework_07_classes/index.js
@@ -96,10 +96,14 @@ function Animal (name) {
     this.say = function () {
         console.log(`неизвестное животное молчит`);
     }
+    this.rename = function (newName) {
+        const regexp = /^[а-яА-ЯёЁ\s-]+$/g;
+        if (newName.match(regexp)) Object.defineProperty(this, 'name', {value: newName});
+    }
 }
 
-function Cat () {
-    Animal.call(this, "Мурка");
+function Cat (name) {
+    Animal.call(this, name);
     this.say = function () {
         console.log(`Кошка говорит`);
     }
@@ -108,45 +112,41 @@ function Cat () {
     }
 }
 
-function Dog () {
-    Animal.call(this, "Тузик");
+function Dog (name) {
+    Animal.call(this, name);
     this.say = function () {
         console.log(`Собака говорит`);
     }
-    this.rename = function (newName) {
-        const regexp = /^[а-яА-ЯёЁ\s-]+$/g;
-        if (newName.match(regexp)) Object.defineProperty(this, 'name', {value: newName});
-    }
 }
 
-function Parrot () {
-    Animal.call(this, "Кеша");
+function Parrot (name) {
+    Animal.call(this, name);
     this.say = function () {
         console.log(`Попугай говорит`);
     }
 }
 
-const cat2 = new Cat();
-const dog2 = new Dog();
-const parrot2 = new Parrot();
+const cat2 = new Cat('Мурочка');
+const dog2 = new Dog('Тузичек');
+const parrot2 = new Parrot('Кешеча');
 
-Object.defineProperty(cat, 'name', {writable: false, configurable: true, enumerable: true});
-Object.defineProperty(cat, 'say', {writable: false, configurable: false, enumerable: false});
-Object.defineProperty(cat, 'eat', {writable: false, configurable: false, enumerable: false});
-Object.defineProperty(cat, 'rename', {writable: false, configurable: false, enumerable: false});
-Object.defineProperty(cat, 'hunt', {writable: false, configurable: false, enumerable: false});
-Object.defineProperty(dog, 'name', {writable: false, configurable: true, enumerable: true});
-Object.defineProperty(dog, 'say', {writable: false, configurable: false, enumerable: false});
-Object.defineProperty(dog, 'eat', {writable: false, configurable: false, enumerable: false});
-Object.defineProperty(dog, 'rename', {writable: false, configurable: false, enumerable: false});
-Object.defineProperty(parrot, 'name', {writable: false, configurable: true, enumerable: true});
-Object.defineProperty(parrot, 'say', {writable: false, configurable: false, enumerable: false});
-Object.defineProperty(parrot, 'eat', {writable: false, configurable: false, enumerable: false});
-Object.defineProperty(parrot, 'rename', {writable: false, configurable: false, enumerable: false});
+Object.defineProperty(cat2, 'name', {writable: false, configurable: true, enumerable: true});
+Object.defineProperty(cat2, 'say', {writable: false, configurable: false, enumerable: false});
+Object.defineProperty(cat2, 'eat', {writable: false, configurable: false, enumerable: false});
+Object.defineProperty(cat2, 'rename', {writable: false, configurable: false, enumerable: false});
+Object.defineProperty(cat2, 'hunt', {writable: false, configurable: false, enumerable: false});
+Object.defineProperty(dog2, 'name', {writable: false, configurable: true, enumerable: true});
+Object.defineProperty(dog2, 'say', {writable: false, configurable: false, enumerable: false});
+Object.defineProperty(dog2, 'eat', {writable: false, configurable: false, enumerable: false});
+Object.defineProperty(dog2, 'rename', {writable: false, configurable: false, enumerable: false});
+Object.defineProperty(parrot2, 'name', {writable: false, configurable: true, enumerable: true});
+Object.defineProperty(parrot2, 'say', {writable: false, configurable: false, enumerable: false});
+Object.defineProperty(parrot2, 'eat', {writable: false, configurable: false, enumerable: false});
+Object.defineProperty(parrot2, 'rename', {writable: false, configurable: false, enumerable: false});
 
-console.log(Object.getOwnPropertyDescriptors(cat));
-console.log(Object.getOwnPropertyDescriptors(dog));
-console.log(Object.getOwnPropertyDescriptors(parrot));
+console.log(Object.getOwnPropertyDescriptors(cat2));
+console.log(Object.getOwnPropertyDescriptors(dog2));
+console.log(Object.getOwnPropertyDescriptors(parrot2));
 
 cat2.eat();
 cat2.say();
@@ -166,3 +166,64 @@ dog2.eat();
 
 console.log('\nДомашнее задание 7. Часть 3 (Классы)');
 
+
+class Animals {
+    #name
+    constructor (name) {
+        this.#name = name;
+    }
+
+    eat () {
+        console.log(`${this.#name} ест`);
+    }
+    say () {
+        console.log(`неизвестное животное молчит`);
+    }
+    get name() {
+        return this.#name
+    }
+    rename (newName) {
+        const regexp = /^[а-яА-ЯёЁ\s-]+$/g;
+        if (newName.match(regexp)) this.#name = newName;
+    }
+}
+
+class Cats extends Animals{
+    say () {
+        console.log(`Кошка говорит`);
+    }
+    hunt () {
+        console.log(`${this.name} охотится`);
+    }
+}
+
+class Dogs extends Animals {
+    say () {
+        console.log(`Собака говорит`);
+    }
+}
+
+class Parrots extends Animals {
+    say () {
+        console.log(`Попугай говорит`);
+    }
+}
+
+const cat3 = new Cats('Кузя');
+const dog3 = new Dogs('Шарик');
+const parrot3 = new Parrots('Гоша');
+
+console.log(Object.getOwnPropertyDescriptors(cat3));
+console.log(Object.getOwnPropertyDescriptors(dog3));
+console.log(Object.getOwnPropertyDescriptors(parrot3));
+
+cat3.eat();
+cat3.say();
+cat3.hunt();
+dog3.eat();
+dog3.say();
+parrot3.eat();
+parrot3.say();
+
+dog3.rename('Полкан');
+dog3.eat();
