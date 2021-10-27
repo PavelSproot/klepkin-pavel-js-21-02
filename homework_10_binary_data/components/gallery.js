d3157f5c1de79261952c599c3a590745d81f6b02
PavelSproot <forregs@inbox.ru>
2021-10-27 22:35:08 +1000

    Добавлен лоадер с анимацией на JS. Исправлены мелкие недочеты.

--- homework_10_binary_data/components/gallery.js
+++ homework_10_binary_data/components/gallery.js
@@ -1,28 +1,29 @@
 import {imgbbApi} from "../api/imgbbapi.js";
 import {localStorageImages} from "../localStorage/localStorage.js";
+import {Loader} from "./loader.js";
 
 export const imageGallery = {
     initiate() {
         this.mainContainer = document.createElement('div');
+        this.header = document.createElement('h3');
+        this.loaderContainer = document.createElement('div');
         this.uploadFile = document.createElement('input');
         this.uploadButton = document.createElement('input');
         this.imageList = document.createElement('div');
-        this.header = document.createElement('h3');
 
+        this.mainContainer.classList.add('gallery');
         this.imageList.classList.add('gallery__imageList');
+        this.loaderContainer.classList.add('gallery__loader');
         this.header.innerText = 'Галерея';
         this.uploadFile.type = 'file';
         this.uploadButton.type = 'button';
         this.uploadButton.value = 'Отправить'
         this.uploadButton.addEventListener('click', () => {
-            const reader = new FileReader();
-            reader.readAsDataURL(this.uploadFile.files[0]);
-            reader.onload = () => {
-                imgbbApi.uploadFileToImgbb(reader.result, (res) => this.uploadFileBase64(res), () => this.doErrorMessage());
-            }
+            this.uploadFileBase64();
         });
 
         this.mainContainer.append(this.header);
+        this.mainContainer.append(this.loaderContainer);
         this.mainContainer.append(this.uploadFile);
         this.mainContainer.append(this.uploadButton);
         this.mainContainer.append(this.imageList);
@@ -36,6 +37,10 @@ export const imageGallery = {
         link.type = 'text/css';
         link.href = './css/gallery.css';
         document.head.append(link);
+
+        this.loader = new Loader();
+        this.loader.insertInTo(this.loaderContainer);
+        this.loaderContainer.style.height = this.loader.loaderHeight + "px";
     },
     displayGalleryImage (base64img) {
         const img = document.createElement('img');
@@ -47,13 +52,19 @@ export const imageGallery = {
         elem.append(this.mainContainer);
     },
     handleResult (res) {
+        this.loader.displayLoader(false);
+        this.loader.stopAnimation();
         localStorageImages.addItemToStorage(res.data.display_url);
         this.displayGalleryImage(res.data.display_url);
     },
     doErrorMessage () {
+        this.loader.displayLoader(false);
+        this.loader.stopAnimation();
         console.log(`Чтото пошло не так...`)
     },
     uploadFileBase64 () {
+        this.loader.displayLoader(true);
+        this.loader.doAnimation();
         const reader = new FileReader();
         reader.readAsDataURL(this.uploadFile.files[0]);
         reader.onload = () => {
