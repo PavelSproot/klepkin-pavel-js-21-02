68b85c704c8143cbd687f1bd9eb73a9ef85ed553
PavelSproot <forregs@inbox.ru>
2021-11-20 00:43:32 +1000

    Файл один подбитый был...

--- homework_13_react/react-app/src/components/popularItems/PopularItemsElement.tsx
+++ homework_13_react/react-app/src/components/popularItems/PopularItemsElement.tsx
@@ -5,7 +5,7 @@ export class PopularItemsElement extends React.Component<any, any> {
     render () : ReactNode {
         return (
             <div className="popular-items__item-card popular-items__item-card_theme-dark">
-                <div className="popular-items__pict">
+                <div className="popular-items__pict"></div>
                 <div className="popular-items__itemdescr">
                     <div className="popular-items__itemname"><a href="">{this.props.name}</a></div>
                     <div className="popular-items__itembut"><a href="" className="popular-items__ref">Купить</a>
